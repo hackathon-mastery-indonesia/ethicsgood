d0342f36be9a73e4e90b3dd74bd847632b048be4
isaui <isadestroyed17@gmail.com>
2024-02-09 15:29:27 +0700

    yyeee

--- app/layout/SidebarLayout.tsx
+++ app/layout/SidebarLayout.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from "react";
 import Sidebar, { ArticleSidebar } from "../component/Sidebar/Sidebar";
 import { SidebarProvider, useSidebar } from "../context/SidebarContext";
